76168145d092a4d50aec3f07677608739e514c6a
grodriguezbarrero <gonzalorodbarrero@gmail.com>
2023-06-06 01:41:13 +0200

    NEW SIMULATION WITH 24 SCENARIOS (1h30); added some functions to help print cool graphs

---
+++ fun_graphWGs.m
@@ -0,0 +1,61 @@
+function fun_graphWGs(igenscenario, igenonline, i_delta_vw, i_t_delta_vw, c_t_wg, c_w_wg, c_pufls_wg, c_WGpenetration_wg, c_pgenWGtot_wg, c_pgentot_wg, v_colours)
+
+% This function shows several signals (including the frequency) for three
+% different WG penetrations.
+
+hf = figure('WindowState','maximized');
+subplot(5,1,1);
+
+for WGgroupsonline = 1:3
+    plot(c_t_wg{igenscenario, igenonline, WGgroupsonline, i_delta_vw, i_t_delta_vw}, ...
+        c_w_wg{igenscenario, igenonline, WGgroupsonline, i_delta_vw, i_t_delta_vw}, ...
+        'Color',v_colours(WGgroupsonline));hold on;
+end
+legend('3 WGs', '6 WGs', '9 WGs');
+ylabel('Freq \omega (Hz)')
+hold off;
+
+subplot(5, 1, 2);
+
+for WGgroupsonline = 1:3
+    plot(c_t_wg{igenscenario, igenonline, WGgroupsonline, i_delta_vw, i_t_delta_vw}, ...
+        c_pufls_wg{igenscenario, igenonline, WGgroupsonline, i_delta_vw, i_t_delta_vw}, ...
+        'Color',v_colours(WGgroupsonline));hold on;
+end
+ylabel('P_{shedded}^{UFLS} (MW)')
+hold off;
+
+subplot(5, 1, 3);
+
+for WGgroupsonline = 1:3
+    plot(c_t_wg{igenscenario, igenonline, WGgroupsonline, i_delta_vw, i_t_delta_vw}, ...
+        c_WGpenetration_wg{igenscenario, igenonline, WGgroupsonline, i_delta_vw, i_t_delta_vw}, ...
+        'Color',v_colours(WGgroupsonline));hold on;
+end
+ylabel('WG pen. (%)')
+hold off;
+
+subplot(5, 1, 4);
+
+for WGgroupsonline = 1:3
+    plot(c_t_wg{igenscenario, igenonline, WGgroupsonline, i_delta_vw, i_t_delta_vw}, ...
+        c_pgenWGtot_wg{igenscenario, igenonline, WGgroupsonline, i_delta_vw, i_t_delta_vw}, ...
+        'Color',v_colours(WGgroupsonline));hold on;
+end
+ylabel('P_{gen}^{WG} (MW)')
+hold off;
+
+subplot(5, 1, 5);
+
+for WGgroupsonline = 1:3
+    plot(c_t_wg{igenscenario, igenonline, WGgroupsonline, i_delta_vw, i_t_delta_vw}, ...
+        c_pgentot_wg{igenscenario, igenonline, WGgroupsonline, i_delta_vw, i_t_delta_vw}, ...
+        'Color',v_colours(WGgroupsonline));hold on;
+end
+xlabel('Time (s)')
+ylabel('P_{gen}^{tot} (MW)')
+hold off;
+
+sgt = sgtitle(['Scenario ', num2str(igenscenario), ' with the number ', num2str(igenonline), ' Bus shut off'],'Color',"#0072BD", 'interpreter','latex');
+sgt.FontSize = 18;
+
